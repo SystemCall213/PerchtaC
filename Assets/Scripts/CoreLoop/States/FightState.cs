e56dd22713621dcc08786323c3b0409e8d3b0fd5
gonerik <s28728@pjwstk.edu.pl>
2026-03-26 19:45:21 +0100

    FIghtState

---
+++ Assets/Scripts/CoreLoop/States/FightState.cs
@@ -0,0 +1,25 @@
+﻿using CoreLoop.Interfaces;
+using Zenject;
+
+namespace CoreLoop.States
+{
+    public class FightState : State<string>
+    {
+        [Inject] private readonly IGameStateMachine gameStateMachine;
+        [Inject] private readonly ISceneLoader sceneLoader;
+        public FightState(string payload)
+        {
+            Payload = payload;
+        }
+        public override void Enter()
+        {
+            sceneLoader.LoadCombatScene(Payload);
+            
+        }
+
+        public override void Exit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
