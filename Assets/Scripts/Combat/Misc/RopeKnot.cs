09ea73d8da47e1e9a1e128a792289be540c13594
gonerik <163130399+gonerik@users.noreply.github.com>
2026-05-12 13:06:34 +0200

    Rope Attack init

---
+++ Assets/Scripts/Combat/Misc/RopeKnot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Combat.Arena;
+using UnityEngine;
+
+namespace Combat.Misc
+{
+    public class RopeKnot : MonoBehaviour
+    {
+        [SerializeField] private float lifetime = 10f;
+        [SerializeField] private GameObject rope;
+        [SerializeField] private int radialDelta = 5;
+        [SerializeField] private float delayBetweenSpawns = 0.5f;
+        
+        private int currentRadialIndex = 0;
+        
+    }
+}
---
+++ Assets/Scripts/Combat/Misc/RopeStrech.cs
@@ -0,0 +1,33 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Combat.Misc
+{
+    public class RopeStrech : MonoBehaviour
+    {
+        [SerializeField] private float strechTime = 1f;
+        [SerializeField] private float endScale = 1f;
+        
+        private float startScale;
+        private void Start()
+        {
+            startScale = transform.localScale.y;
+            StrechOut();
+        }
+        private void StrechOut()
+        {
+            Sequence fallSequence = DOTween.Sequence();
+            fallSequence.Append(transform.DOScaleY(endScale, strechTime));
+            fallSequence.Join(transform.DOScaleX(1/endScale, strechTime).OnComplete(StrechIn));
+            fallSequence.Play();
+        }
+
+        private void StrechIn()
+        {
+            Sequence fallSequence = DOTween.Sequence();
+            fallSequence.Append(transform.DOScaleY(startScale, strechTime));
+            fallSequence.Join(transform.DOScaleX(startScale, strechTime));
+            fallSequence.Play();
+        }
+    }
+}
