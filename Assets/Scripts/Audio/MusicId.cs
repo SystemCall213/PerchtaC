0a2251b3bea776ea33d0ee96d8b223913b0ec310
gonerik <163130399+gonerik@users.noreply.github.com>
2026-05-17 02:22:04 +0200

    Music for cinematics

--- Assets/Scripts/Audio/MusicId.cs
+++ Assets/Scripts/Audio/MusicId.cs
@@ -4,6 +4,7 @@
     {
         None = 0,
         MainMenu = 1,
+        Cinematic = 2,
         
         Room1 = 11,
         Room2 = 12,
--- Assets/Scripts/GameStateMachine/States/CinematicState.cs
+++ Assets/Scripts/GameStateMachine/States/CinematicState.cs
@@ -11,20 +11,23 @@ namespace CoreLoop.States
         private readonly MusicId cinematicMusic;
         private readonly ISceneLoader sceneLoader;
         private readonly DefaultActions defaultActions;
+        private readonly IMusicService musicService;
 
         [Inject]
-        public CinematicState(CinematicStatePayload payload, ISceneLoader sceneLoader, DefaultActions defaultActions)
+        public CinematicState(CinematicStatePayload payload, ISceneLoader sceneLoader, DefaultActions defaultActions, MusicService musicService)
         {
             Payload = payload;
             cinematicMusic = payload.MusicId;
             this.sceneLoader = sceneLoader;
             this.defaultActions = defaultActions;
+            this.musicService = musicService;
         }
 
         public override void Enter()
         {
             defaultActions.UI.CloseMenu.Disable();
             defaultActions.UI.SkipCinematic.Enable();
+            musicService.Request(cinematicMusic);
         }
 
         public override void Exit()
